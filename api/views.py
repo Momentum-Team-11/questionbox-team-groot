9baa91db4eea09da16775ccc400bfbab51e8cfef
Emily Starr <emilyflo.starr@gmail.com>
2022-04-10 15:34:34 -0400

    fixed a typo from serAILizer class

--- api/views.py
+++ api/views.py
@@ -20,4 +20,4 @@ class QuestionDetailsView(generics.RetrieveUpdateDestroyAPIView):
 
 class QuestionResponseView(generics.ListCreateAPIView):
     queryset = Response.objects.all()
-    serailizer_class = ResponseSerializer
+    serializer_class = QuestionResponseSerializer
